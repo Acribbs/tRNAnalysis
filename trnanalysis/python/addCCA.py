4e2f7402f9eeb9264687b899bf19313f905bb3cd
Acribbs <adam.cribbs@imm.ox.ac.uk>
2019-12-25 08:31:07 +0000

    fixed the regular expression

--- trnanalysis/python/addCCA.py
+++ trnanalysis/python/addCCA.py
@@ -67,13 +67,14 @@ def main(argv=None):
     for cur_record in iterator:
 
 
-        #key = cur_record.title
         # This is a temp fix because bedtools getfasta --name seems to have
         # changed the way it names the fasta titles. This may be temp but This
         # will fix this issue for the time being.
-        m = re.match("(chr\d+.tRNA\d+-\S+)-::\S+[+|-]", cur_record.title.replace("(","").replace(")",""))
+        m = re.match("(chr\d+.tRNA\d+-\S+)-\S*::\S+[+|-]", cur_record.title.replace("(","").replace(")",""))
 
-        key = m.group(0)
+        key = str(m.group(1))
+        #key = cur_record.title
+
         if "pseudo" in key:
             pass
 
